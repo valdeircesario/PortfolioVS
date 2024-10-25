fad856ac81ee95a1eabe69ae9728db879e1be84b
valdeir cesario <135670289+valdeircesario@users.noreply.github.com>
2024-10-25 00:18:28 -0300

    acrescentado compomente

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -57,37 +57,50 @@ function Sobremim() {
     
 
       <p ref={(el) => (paragraphsRef.current[0] = el)} className={Styles.P1}>
-        Durante minha trajetória de estudos no Instituto Federal, adquiri conhecimentos em diversas áreas, como lógica de programação, gerenciamento de projetos, levantamento e análise de requisitos, administração empresarial e metodologias ágeis, como Scrum e Kanban. Também me especializei em estrutura de dados, linguagem de programação Java, linguagem de manipulação de dados SQL e desenvolvimento web, entre outros.
-      </p>
 
-      <p ref={(el) => (paragraphsRef.current[1] = el)} className={Styles.P2}>
-        Ao longo da minha jornada na área de tecnologia, tive a oportunidade de desenvolver projetos e trabalhos que aplicaram os conhecimentos adquiridos. Além disso, busquei constantemente aprimorar minhas habilidades, participando de cursos presenciais e online, incluindo na área de soft skills, como trabalho em equipe.
-      </p>
+Sou um profissional de TI com uma sólida formação no Instituto Federal, onde desenvolvi habilidades em lógica de programação, gerenciamento de projetos e metodologias ágeis, como Scrum e Kanban. Minha especialização abrange Java, SQL, desenvolvimento web e estrutura de dados.
 
-      <p ref={(el) => (paragraphsRef.current[2] = el)} className={Styles.P3}>
-        Tenho experiência no uso de diversas ferramentas e tecnologias, como Visual Studio Code, IntelliJ, PgAdmin, GitHub, Java, JavaScript, PHP, HTML, CSS, React, Angular , Quarkus e Bootstrap. Também possuo conhecimento em bancos de dados, especialmente PostgreSQL. Ao longo da minha trajetória, adquiri certificados que comprovam minha capacitação e qualificação.
-      </p>
+</p>
 
-      <p ref={(el) => (paragraphsRef.current[3] = el)} className={Styles.P3}>
-        Meu portfólio contém informações sobre meus projetos, experiências, cursos concluídos e meus contatos, permitindo que você conheça melhor meu trabalho. Estou em busca de uma oportunidade onde eu possa contribuir em equipe, aplicar meus conhecimentos e continuar aprendendo. Meu objetivo no mercado de trabalho é crescer profissionalmente e ajudar minha equipe a atingir seus objetivos.
-      </p>
+<p ref={(el) => (paragraphsRef.current[1] = el)} className={Styles.P2}>
+
+Tenho experiência prática em projetos que aplicam meus conhecimentos e busco sempre aprimorar minhas habilidades, participando de cursos presenciais e online, incluindo temas de soft skills, como trabalho em equipe.
+
+</p>
+
+<p ref={(el) => (paragraphsRef.current[2] = el)} className={Styles.P3}>
+
+Sou proficiente em ferramentas como Visual Studio Code, IntelliJ, GitHub e bancos de dados, especialmente PostgreSQL. Minhas habilidades técnicas incluem Java, JavaScript, PHP, HTML, CSS, React, Angular, Quarkus e Bootstrap, respaldadas por certificados que atestam minha capacitação.
+
+</p>
+
+<p ref={(el) => (paragraphsRef.current[3] = el)} className={Styles.P3}>
+
+Meu portfólio apresenta meus projetos, experiências e contatos, refletindo meu compromisso em contribuir para o sucesso da equipe e crescer profissionalmente. Estou pronto para novas oportunidades onde possa aplicar meu conhecimento e fazer a diferença.
+
+</p>
 
       <Faixa/>
 
       <div className={Styles.icons}>
         <h3>Aqui Estão Algumas linguagens e ferramentas que mais uso e tenho conhecimentos</h3>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEc9A_S6BPxCDRp5WjMFEfXrpCu1ya2OO-Lw&s" alt="Icone de Ferramenta 1" />
-        <img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/css_original_wordmark_logo_icon_146576.png" alt="Logo CSS" />
-        <img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/java_original_wordmark_logo_icon_146459.png" alt="Logo Java" />
-        <img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/javascript_original_logo_icon_146455.png" alt="Logo JavaScript" />
-        <img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_wordmark_logo_icon_146375.png" alt="Logo React" />
-        <img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/nodejs_original_wordmark_logo_icon_146412.png" alt="Logo Node.js" />
-        <img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/github_original_wordmark_logo_icon_146506.png" alt="Logo GitHub" />
-        <img src="https://cdn.icon-icons.com/icons2/9/PNG/256/sql_racer_gamedatabase_sql_1526.png" alt="Logo SQL" />
-        <img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/postgresql_plain_wordmark_logo_icon_146390.png" alt="Logo PostgreSQL" />
-        <img src="https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_vscode_icon_130084.png" alt="Logo Visual Studio Code" />
-        <img src="https://cdn.icon-icons.com/icons2/3053/PNG/512/intellij_macos_bigsur_icon_190061.png" alt="Logo IntelliJ" />
-        <img src="https://th.bing.com/th/id/OIP.0ZHNxzjEbkekjFZaVOF2IwHaHa?rs=1&pid=ImgDetMain" alt="Logo Angular" />
+        <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" alt="HTML5" />
+        <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white" />
+        <img src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white" alt="Java" />
+        <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" alt="JavaScript" />
+        <img src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="React" />
+        <img src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white" alt="NodeJS" />
+        <img src="https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white" alt="Git" />
+        <img src="https://img.shields.io/badge/mysql-4479A1.svg?style=for-the-badge&logo=mysql&logoColor=white" alt="MySQL" />
+        <img src="https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white" alt="Postgres" />
+        <img src="https://img.shields.io/badge/VSCode-%23007ACC.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white" />
+        <img src="https://img.shields.io/badge/Quarkus-%23000000.svg?style=for-the-badge&logo=quarkus&logoColor=white" />
+        <img src="https://img.shields.io/badge/angular-%23DD0031.svg?style=for-the-badge&logo=angular&logoColor=white" alt="Angular" />
+        
+        <img src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white" />
+        <img src="https://img.shields.io/badge/IntelliJ-%23000000.svg?style=for-the-badge&logo=intellijidea&logoColor=white" alt="IntelliJ" />
+        <img src="https://img.shields.io/badge/netlify-%23000000.svg?style=for-the-badge&logo=netlify&logoColor=#00C7B7" />
+        <img src="https://img.shields.io/badge/pgAdmin-%233B4D8D.svg?style=for-the-badge&logo=postgresql&logoColor=white" alt="pgAdmin" />
       </div>
     </div>
   );
